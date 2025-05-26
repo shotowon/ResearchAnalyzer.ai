c82e1011ca4338d0800e7af7959814c4e0955153
shotowon <sanzhar.maratov@proton.me>
2025-05-26 15:18:41 -0400

    init services

--- backend/src/init.py
+++ backend/src/init.py
@@ -10,9 +10,13 @@ from src.config import config
 from src.storage.postgres.user_acct_storage import UserAcctStorage
 from src.storage.postgres.auth_token_storage import AuthTokenStorage
 from src.storage.postgres.file_mapping_storage import MappingStorage
+from src.storage.minio.file_storage import ArticleStorage
+from src.storage.postgres.chat_storage import ChatStorage
+from src.storage.postgres.message_storage import MessageStorage
+from src.storage.postgres.summary_storage import SummaryStorage
 from src.services.user_accts import UserAcctService
 from src.services.auth_mailing import MailService as AuthMailService
-
+from src.services.documents import DocumentService
 
 config_path = os.getenv("RAI_CFG")
 if config_path is None:
@@ -20,6 +24,7 @@ if config_path is None:
 
 cfg = config.load(config_path)
 setup_logger(cfg.env)
+pgpt_client = PrivateGPTApi(base_url=cfg.pgpt.url)
 
 logger = logging.getLogger("rai")
 
@@ -35,14 +40,25 @@ minio = Minio(
 token_storage = AuthTokenStorage(db=db)
 user_storage = UserAcctStorage(db=db)
 mapping_storage = MappingStorage(db=db)
+article_storage = ArticleStorage(minio)
+chat_storage = ChatStorage(db=db)
+message_storage = MessageStorage(db=db)
+summary_storage = SummaryStorage(db=db)
 
 user_acct_service = UserAcctService(
     user_storage=user_storage,
     token_storage=token_storage,
     config=cfg.auth,
 )
+document_service = DocumentService(
+    mapping_storage=mapping_storage,
+    article_storage=article_storage,
+    gpt_api=pgpt_client,
+    chat_storage=chat_storage,
+    message_storage=message_storage,
+    summary_storage=summary_storage,
+)
 
 auth_mail_service = AuthMailService(config=cfg.auth_mailer)
 
 summary_store = {}
-pgpt_client = PrivateGPTApi(base_url=cfg.pgpt.url)
