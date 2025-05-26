3a64225d7b4fe95ddfeef1db877de3d7b3dd99ca
shotowon <sanzhar.maratov@proton.me>
2025-05-26 15:21:54 -0400

    remove cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -29,11 +29,14 @@ init_db()
 
 
 app = FastAPI()
+
+# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # for frontend
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(router=router)
